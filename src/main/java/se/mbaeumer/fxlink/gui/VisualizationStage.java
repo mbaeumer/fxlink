87d5386b54aab6b5563ceeb34d79e89b09506a90
Martin Bäumer <martin.baeumer@gmail.com>
2022-03-26 00:57:30 +0100

    added word count chart

--- src/main/java/se/mbaeumer/fxlink/gui/VisualizationStage.java
+++ src/main/java/se/mbaeumer/fxlink/gui/VisualizationStage.java
@@ -10,6 +10,7 @@ import javafx.scene.chart.*;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Slider;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -19,7 +20,10 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import se.mbaeumer.fxlink.api.CategoryHandler;
 import se.mbaeumer.fxlink.api.LinkHandler;
+import se.mbaeumer.fxlink.api.WordCountHandler;
 import se.mbaeumer.fxlink.handlers.*;
+import se.mbaeumer.fxlink.util.LinkSplitter;
+import se.mbaeumer.fxlink.util.URLHelper;
 
 import java.util.List;
 import java.util.Map;
@@ -30,17 +34,21 @@ public class VisualizationStage extends Stage {
     private ScrollPane scrollPane;
     private FlowPane flowGeneral;
     private FlowPane flowNumbers;
-    private FlowPane flowBarChart;
+    private FlowPane flowCategoryCount;
+    private FlowPane flowWordCount;
     private ComboBox<String> cmbMinCategoryCount;
     private Label lblLinks;
     private Label lblCategories;
     private BarChart<String,Number> bcCategory;
+    private Slider sliderWordCount;
+    private BarChart<String, Number> bcWordCount;
 
     private FlowPane flowPieChart;
     private BarChart<Object, Long> bcHour;
 
     private LinkHandler linkHandler;
     private CategoryHandler categoryHandler;
+    private WordCountHandler wordCountHandler;
     private Map<Integer, String> weekdays = Map.ofEntries(
             Map.entry(1, "Sunday"),
             Map.entry(2, "Monday"),
@@ -59,7 +67,7 @@ public class VisualizationStage extends Stage {
         this.initScene();
 
         ChangeListener<Number> widthListener = (observable, old, newValue) -> {
-            initSizes();
+            adjustSizes();
         };
         this.widthProperty().addListener(widthListener);
         this.heightProperty().addListener(widthListener);
@@ -92,9 +100,12 @@ public class VisualizationStage extends Stage {
         this.initNumberFlowPane();
         this.initLinkLabel();
         this.initCategoryLabel();
-        this.initBarChartFlowPane();
+        this.initCategoryCountFlowPane();
         this.initComboBox();
         this.initCategoryBarChart(0);
+        this.initWordCountFlowPane();
+        this.initWordCountSlider();
+        this.initWordCountBarChart(0);
         this.initPieChartFlowPane();
         this.initWeekdayPieChart();
         this.initHourBarChart();
@@ -106,6 +117,7 @@ public class VisualizationStage extends Stage {
                 new LinkCreationDBHandler(), new LinkUpdateDBHandler(), new LinkDeletionDBHandler());
         this.categoryHandler = new CategoryHandler(new CategoryReadDBHandler(), new CategoryCreationDBHandler(),
                 new CategoryUpdateDBHandler(), new CategoryDeletionDBHandler(), new LinkUpdateDBHandler());
+        this.wordCountHandler = new WordCountHandler(new LinkReadDBHandler(), new LinkSplitter(new URLHelper()));
     }
 
     private void initNumberFlowPane(){
@@ -137,15 +149,15 @@ public class VisualizationStage extends Stage {
         this.flowNumbers.getChildren().add(this.lblCategories);
     }
 
-    private void initBarChartFlowPane(){
-        this.flowBarChart = new FlowPane();
-        this.flowBarChart.setOrientation(Orientation.HORIZONTAL);
-        this.flowBarChart.setHgap(5);
-        this.flowBarChart.setVgap(10);
-        this.flowBarChart.setPadding(new Insets(10, 10, 5, 10));
-        this.flowBarChart.setEffect(this.createShadow());
-        this.flowBarChart.setBackground(createBackground());
-        this.flowGeneral.getChildren().add(this.flowBarChart);
+    private void initCategoryCountFlowPane(){
+        this.flowCategoryCount = new FlowPane();
+        this.flowCategoryCount.setOrientation(Orientation.HORIZONTAL);
+        this.flowCategoryCount.setHgap(5);
+        this.flowCategoryCount.setVgap(10);
+        this.flowCategoryCount.setPadding(new Insets(10, 10, 5, 10));
+        this.flowCategoryCount.setEffect(this.createShadow());
+        this.flowCategoryCount.setBackground(createBackground());
+        this.flowGeneral.getChildren().add(this.flowCategoryCount);
     }
 
     private void initComboBox(){
@@ -159,7 +171,7 @@ public class VisualizationStage extends Stage {
         });
         this.cmbMinCategoryCount.getSelectionModel().selectFirst();
 
-        this.flowBarChart.getChildren().add(this.cmbMinCategoryCount);
+        this.flowCategoryCount.getChildren().add(this.cmbMinCategoryCount);
     }
 
     private void handleComboBoxChange(final String value){
@@ -177,7 +189,7 @@ public class VisualizationStage extends Stage {
         }
 
         this.initCategoryBarChart(min);
-        this.initSizes();
+        this.adjustSizes();
     }
 
     private void initPieChartFlowPane(){
@@ -194,9 +206,7 @@ public class VisualizationStage extends Stage {
     }
 
     private void initCategoryBarChart(int minValue){
-        if (this.flowBarChart.getChildren().contains(this.bcCategory)){
-            this.flowBarChart.getChildren().remove(this.bcCategory);
-        }
+        this.flowCategoryCount.getChildren().remove(this.bcCategory);
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
         this.bcCategory = new BarChart<>(xAxis,yAxis);
@@ -213,8 +223,68 @@ public class VisualizationStage extends Stage {
 
         series1.setName("Number of links per category");
         bcCategory.getData().addAll(series1);
-        this.flowBarChart.getChildren().add(bcCategory);
+        this.flowCategoryCount.getChildren().add(bcCategory);
     }
+
+    private void initWordCountFlowPane(){
+        this.flowWordCount = new FlowPane();
+        this.flowWordCount.setOrientation(Orientation.HORIZONTAL);
+        this.flowWordCount.setHgap(5);
+        this.flowWordCount.setVgap(10);
+        this.flowWordCount.setPadding(new Insets(10, 10, 5, 10));
+        this.flowWordCount.setEffect(this.createShadow());
+        this.flowWordCount.setBackground(createBackground());
+        this.flowGeneral.getChildren().add(this.flowWordCount);
+    }
+
+    private void initWordCountSlider(){
+        this.sliderWordCount = new Slider();
+
+        this.sliderWordCount.setMin(0);
+        this.sliderWordCount.setMax(100);
+        this.sliderWordCount.setValue(5);
+        this.sliderWordCount.setMajorTickUnit(10);
+        this.sliderWordCount.setShowTickLabels(true);
+
+        this.sliderWordCount.valueProperty().addListener((observable, oldValue, newValue) -> {
+            XYChart.Series wordCountSeries = this.createWordCountSeries(newValue.intValue());
+            this.bcWordCount.getData().setAll(wordCountSeries);
+        });
+
+        this.flowWordCount.getChildren().add(this.sliderWordCount);
+    }
+
+    private void initWordCountBarChart(int minWordCount){
+        this.flowWordCount.getChildren().remove(this.bcWordCount);
+        final CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setCenterShape(true);
+        xAxis.setAnimated(false);
+        final NumberAxis yAxis = new NumberAxis();
+        this.bcWordCount = new BarChart<>(xAxis,yAxis);
+        this.bcWordCount.getData().clear();
+        this.bcWordCount.getData().addAll(createWordCountSeries(minWordCount));
+        this.flowWordCount.getChildren().add(bcWordCount);
+    }
+
+    private XYChart.Series createWordCountSeries(int min){
+        XYChart.Series series1 = new XYChart.Series();
+
+        Map<String, Integer> allCategoryCounts = this.wordCountHandler.getWordCount();
+        Map<String, Integer> filteredWordCount = allCategoryCounts.entrySet().stream()
+                .filter(stringIntegerEntry -> stringIntegerEntry.getValue() >= min)
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue
+                        ));
+
+        filteredWordCount.entrySet().stream()
+                .forEach(stringIntegerEntry -> series1.getData().add(new XYChart.Data(stringIntegerEntry.getKey(), stringIntegerEntry.getValue())));
+        series1.setName("Word count");
+
+        return series1;
+    }
+
+
 
     private void initWeekdayPieChart(){
         PieChart pieChart = new PieChart();
@@ -261,20 +331,45 @@ public class VisualizationStage extends Stage {
         return new Background(bgFill);
     }
 
-    private void initSizes(){
+    private void adjustSizes(){
         System.out.println("Scroll pane: " + scrollPane.getWidth());
         System.out.println("Flow pane: " + flowGeneral.getWidth());
-        System.out.println("Flow barchart: " + flowBarChart.getWidth());
-        System.out.println("Flow barchart pref: " + flowBarChart.getPrefWidth());
+        System.out.println("Flow barchart: " + flowCategoryCount.getWidth());
+        System.out.println("Flow barchart pref: " + flowCategoryCount.getPrefWidth());
         System.out.println("barchart: " + bcCategory.getWidth());
         this.flowGeneral.setPrefWidth(this.scrollPane.getWidth() - 15);
         this.flowNumbers.setPrefWidth(this.flowGeneral.getWidth()-15);
         this.lblLinks.setPrefWidth((this.flowNumbers.getWidth()-20.0)/2.0);
         this.lblCategories.setPrefWidth((this.flowNumbers.getWidth()-20.0)/2.0);
-        this.flowBarChart.setPrefWidth(this.flowGeneral.getWidth()-15);
+        this.flowCategoryCount.setPrefWidth(this.flowGeneral.getWidth()-15);
+        this.flowWordCount.setPrefWidth(this.flowGeneral.getWidth()-15);
         this.flowPieChart.setPrefWidth(this.flowGeneral.getWidth()-15);
-        this.bcCategory.setPrefWidth(this.flowBarChart.getWidth()-15);
+        this.bcCategory.setPrefWidth(this.flowCategoryCount.getWidth()-15);
+        this.sliderWordCount.setPrefWidth(this.flowWordCount.getWidth()-15);
+        this.bcWordCount.setPrefWidth(this.flowWordCount.getWidth()-15);
         System.out.println("barchart: " + bcCategory.getWidth());
-        this.cmbMinCategoryCount.setPrefWidth(this.flowBarChart.getWidth()-15);
+        this.cmbMinCategoryCount.setPrefWidth(this.flowCategoryCount.getWidth()-15);
     }
+
+    private void initSizes(){
+        System.out.println("Scroll pane: " + scrollPane.getWidth());
+        System.out.println("Flow pane: " + flowGeneral.getWidth());
+        System.out.println("Flow barchart: " + flowCategoryCount.getWidth());
+        System.out.println("Flow barchart pref: " + flowCategoryCount.getPrefWidth());
+        System.out.println("barchart: " + bcCategory.getWidth());
+        this.flowNumbers.setPrefWidth(this.flowGeneral.getWidth()-15);
+        this.lblLinks.setPrefWidth((this.flowNumbers.getWidth()-20.0)/2.0);
+        this.lblCategories.setPrefWidth((this.flowNumbers.getWidth()-20.0)/2.0);
+        this.flowCategoryCount.setPrefWidth(this.flowGeneral.getWidth()-15);
+        this.flowWordCount.setPrefWidth(this.flowGeneral.getWidth()-15);
+        this.flowPieChart.setPrefWidth(this.flowGeneral.getWidth()-15);
+        this.bcCategory.setPrefWidth(this.flowCategoryCount.getWidth()-15);
+        this.bcWordCount.setPrefWidth(this.flowWordCount.getWidth()-15);
+        System.out.println("barchart: " + bcCategory.getWidth());
+        this.cmbMinCategoryCount.setPrefWidth(this.flowCategoryCount.getWidth()-15);
+    }
+
+
+
+
 }
