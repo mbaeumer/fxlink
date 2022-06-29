adc9297574a62478fab2a1d7a095e2c878c47caa
Martin Bäumer <martin.baeumer@gmail.com>
2022-06-29 23:09:44 +0200

    added FollowupRankHandler and test

---
+++ src/main/java/se/mbaeumer/fxlink/api/FollowUpRankHandler.java
@@ -0,0 +1,73 @@
+package se.mbaeumer.fxlink.api;
+
+import se.mbaeumer.fxlink.handlers.GenericDBHandler;
+import se.mbaeumer.fxlink.handlers.LinkReadDBHandler;
+import se.mbaeumer.fxlink.handlers.LinkUpdateDBHandler;
+import se.mbaeumer.fxlink.models.Link;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class FollowUpRankHandler {
+    private final LinkReadDBHandler linkReadDBHandler;
+    private final LinkUpdateDBHandler linkUpdateDBHandler;
+    private int lowestRank;
+    private int highestRank;
+    List<Link> linksOrderedByRank;
+
+    public FollowUpRankHandler(LinkReadDBHandler linkReadDBHandler, LinkUpdateDBHandler linkUpdateDBHandler) {
+        this.linkReadDBHandler = linkReadDBHandler;
+        this.linkUpdateDBHandler = linkUpdateDBHandler;
+        try {
+            this.init();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getLowestRank() {
+        return lowestRank;
+    }
+
+    public int getHighestRank() {
+        return highestRank;
+    }
+
+    public List<Link> getLinksOrderedByRank() {
+        return linksOrderedByRank;
+    }
+
+    public void init() throws SQLException {
+        linksOrderedByRank = linkReadDBHandler.getLinksOrderedByRank(GenericDBHandler.getInstance());
+        highestRank = 1;
+        if (linksOrderedByRank.size() == 0) {
+            lowestRank = 1;
+        }else{
+            lowestRank = linksOrderedByRank.size() + 1;
+        }
+    }
+
+    public void updateRanks(final Link link, final int oldRank){
+        if (oldRank != link.getFollowUpRank()){
+            if (oldRank > 0) {
+                linksOrderedByRank.remove(oldRank - 1);
+            }
+
+            int insertIndex = 0;
+            if (linksOrderedByRank.size() > 0){
+                insertIndex = link.getFollowUpRank()-1;
+            }
+            linksOrderedByRank.add(link.getFollowUpRank()-1, link);
+        }
+
+        int rank = 1;
+        for (Link aLink : linksOrderedByRank){
+            try {
+                linkUpdateDBHandler.updateRank(aLink, rank, GenericDBHandler.getInstance());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            rank++;
+        }
+    }
+}
---
+++ src/test/java/se/mbaeumer/fxlink/api/FollowUpRankHandlerTest.java
@@ -0,0 +1,29 @@
+package se.mbaeumer.fxlink.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import se.mbaeumer.fxlink.handlers.LinkReadDBHandler;
+import se.mbaeumer.fxlink.handlers.LinkUpdateDBHandler;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FollowUpRankHandlerTest {
+    private FollowUpRankHandler followUpRankHandler;
+    @Mock
+    private LinkReadDBHandler linkReadDBHandler;
+    @Mock
+    private LinkUpdateDBHandler linkUpdateDBHandler;
+
+    @Before
+    public void setUp(){
+        followUpRankHandler = new FollowUpRankHandler(linkReadDBHandler, linkUpdateDBHandler);
+    }
+
+    @Test
+    public void updateRanks() {
+    }
+}
